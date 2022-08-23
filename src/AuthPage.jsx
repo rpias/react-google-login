a0b7600e171a39524927ae7390b35e0b53dbaecf
Richard Pias <rpias@practia.uy>
2022-08-23 15:59:30 -0300

    actualización

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { gapi } from "gapi-script";
 
@@ -14,20 +14,26 @@ function AuthPage() {
     gapi.load("client:auth2", start);
   }, []);
 
+  const [user, setUser] = useState({});
+  const [isLogin, setIsLogin] = useState(false)
+
   const onSuccess = (response) => {
     console.log("SUCCESS", response);
-    window.location.assign("https://pias.uy/buscar-gif");
+    setUser(response);
+    setIsLogin(true);
+    // window.location.assign("https://pias.uy/buscar-gif");
   };
   const onFailure = (response) => {
     console.log("FAILED", response);
   };
   const onLogoutSuccess = () => {
     console.log("SUCESS LOG OUT");
+    setUser({});
+    setIsLogin(false);
   };
 
   return (
     <div>
-
       <GoogleLogin
         clientId={
           "842322570073-kn7h3v5i6rhuinbbuhq6rmlrovf1bkpc.apps.googleusercontent.com"
@@ -42,6 +48,17 @@ function AuthPage() {
         }
         onLogoutSuccess={onLogoutSuccess}
       />
+
+      <div>
+        {isLogin && <h2>{user.profileObj.name}</h2>}
+        {isLogin && <h3>{user.profileObj.email}</h3>}
+        {isLogin && <h3>{user.profileObj.googleId}</h3>}
+        {isLogin && <img src={user.profileObj.imageUrl} alt="" />}
+      </div>
+
+      <div>
+        {!isLogin && <h2>No hay nadie logueado</h2>}
+      </div>
     </div>
   );
 }
